c2261f27a217e4a52e1826b323590a652f0868e2
Aleksey Pavlov <brian.iproject@gmail.com>
2021-04-21 09:43:52 +0300

    homework-7

---
+++ globals.js
@@ -0,0 +1,5 @@
+const $arenas = document.querySelector('.arenas');
+const $control = document.querySelector('.control');
+const $randomButton = $arenas.querySelector('.button[type=submit]');
+
+export {$arenas, $control, $randomButton};
